b32281f14ef550c2ccc8a214bae1fc4c1ea61315
RichardGabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-10 16:21:53 -0800

    Add basic Express structure

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Inventory Management Express app - listening on port ${PORT}!`);
+});
