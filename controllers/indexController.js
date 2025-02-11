7b8df93b008298b8ad41243267cc0c0531b00248
RichardGabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-02-10 16:48:48 -0800

    Add basic homepage handling to index view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,6 @@ const db = require("../db/queries");
 
 async function getAllCategories(req, res) {
   let categories = await db.getAllCategories();
-  console.log("Categories: ", Categories);
+  console.log("Categories: ", categories);
   res.render("index", { categories: categories });
 }
